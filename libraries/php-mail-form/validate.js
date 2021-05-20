fb862973fad80ae98a811e215174fd3afc1308d9
Aru <aurelie.roue@campus.academy>
2021-05-20 20:41:27 +0200

    add modification on libraries

--- libraries/php-mail-form/validate.js
+++ libraries/php-mail-form/validate.js
@@ -91,7 +91,7 @@ jQuery(document).ready(function($) {
         else var str = $(this).serialize();
         var action = $(this).attr('action');
         if (!action) {
-            action = 'contactform/contactform.php';
+            action = 'contact/contact.php';
         }
 
         var this_form = $(this);
